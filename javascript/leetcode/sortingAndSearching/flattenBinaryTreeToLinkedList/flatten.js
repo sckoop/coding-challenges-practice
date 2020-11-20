4f3a21dced4c2d4b7982cd25424f80dd1f23c816
Sean Koop <sean.c.koop@gmail.com>
2020-11-20 09:45:01 -0500

    Rename folder. Solved flatten

---
+++ javascript/leetcode/sortingAndSearching/flattenBinaryTreeToLinkedList/flatten.js
@@ -0,0 +1,18 @@
+export const flatten = (root) => {
+  if (root === null) return;
+  if (root.left) {
+    // step 1
+    var last = root.left;
+    while (last.right !== null) last = last.right;
+    // step 2
+    var tmp = root.right;
+    // step 3
+    root.right = root.left;
+    // step 4
+    last.right = tmp;
+    // step 5
+    root.left = null;
+  }
+
+  flatten(root.right);
+};
